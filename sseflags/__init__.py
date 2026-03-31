eb157011b429977a79b0ff7598f41f91e022dd64
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2026-03-30 20:05:38 -0600

    v0.3

--- sseflags/__init__.py
+++ sseflags/__init__.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     _ext = False
 
-__version__ = '0.3.dev0'
+__version__ = '0.3'
 
 Flags = TypedDict('Flags', {'daz': bool | None, 'ftz': bool | None},
                   total=False)
