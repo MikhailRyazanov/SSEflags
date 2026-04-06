df66143be9ec75e367356f2056d395f9c8bfe32c
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2026-04-06 14:27:14 -0600

    Reduce .so size

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ from setuptools import setup, Extension
 sys.path.insert(0, '')  # include CWD (missing in build isolation)
 from sseflags import __version__
 
+extra_link_args = None
 if sys.platform == 'win32':  # for MSVC
     extra_compile_args = ['/Os']
 else:  # for GCC and Clang
     extra_compile_args = ['-Os', '-g0']
+    if sys.platform != 'darwin':  # ld on macOS does not support these
+        extra_link_args = ['-s', '-Wl,-z,norelro', '-Wl,-z,noseparate-code']
 ext_modules = [
     # ("Path" below is a workaround for Setuptools bug on Windows,
     #  see https://github.com/pypa/setuptools/issues/5093)
     Extension('sseflags._lib', [Path('sseflags/_lib.pyx')],
               extra_compile_args=extra_compile_args,
+              extra_link_args=extra_link_args,
               define_macros=[('Py_LIMITED_API', 0x030A0000)],  # 0x0B = 11
               py_limited_api=True)
 ]
